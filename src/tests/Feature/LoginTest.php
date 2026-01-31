9f863abd6a186dcce553f771f4a995b0b89759da
ks kanae <s.loo093l.e@gmail.com>
2026-01-31 15:32:36 +0900

    不要クラス削除

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 
 class LoginTest extends TestCase
